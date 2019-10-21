57a76658d93db6af18fa8ef65af2dce10492dec3
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-10-21 10:43:01 +0100

    Add styled links to categories

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { setCategories } from '../actions';
 
 class App extends Component {
@@ -13,8 +14,17 @@ class App extends Component {
 
   render() {
     return(
-      <div className='mw9 center pa4 pt5-ns ph7-l'>
+      <div className='flex flex-column mw9 center pa4 pt5-ns ph7-l'>
         <h1 className='helvetica f-headline lh-solid'>Jeopardy!</h1>
+        {
+          this.props.categories.map(category => {
+            return (
+              <div className='outline w-70 pa3 mr2' key={category.id}>
+                <Link to='/category' className='link dim dark-blue'><h4 className='f4 helvetica'>{category.title}</h4></Link>
+              </div>
+          );
+        })
+      }
       </div>
     );
   }
