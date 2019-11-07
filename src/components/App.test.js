91300d152de633dae81eced9e28fad285d8e1a31
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-11-07 19:20:25 +0000

    Failing tests to debug later

---
+++ src/components/App.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { App } from './App';
+import categories from '../data/fixtures';
+
+configure({ adapter: new Adapter() });
+const props = { categories };
+
+describe('App', () => {
+  const app = shallow(<App {...props} />);
+  console.log("APPPPPP", app.props.categories);
+
+  it('renders title', () => {
+    expect(app.find('h1').text()).toEqual('Jeopardy!');
+  });
+
+  it('renders the correct number of links', () => {
+    expect(app.find('Link').length).toEqual(categories.length);
+  });
+
+  it('generates correct link titles', () => {
+    app.find('Link h4').forEach((linkTitle, index) => {
+      expect(linkTitle.text()).toEqual(categories[index].title);
+    })
+  });
+});
---
+++ src/data/fixtures.js
@@ -0,0 +1,14 @@
+export const categories = [
+  {
+    id: 0,
+    title: 'Alabama'
+  },
+  { 
+    id: 1,
+    title: 'Damage'
+  },
+  {
+    id: 2,
+    title: 'Doubt'
+  }
+]
